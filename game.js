86273f255043f4338a06d75958df7cf6e0cdbd7b
koza212 <kozlowskimaciej46@gmail.com>
2025-04-01 22:29:57 +0200

    wuja zaczal pracowac kurwy (grawitacja

---
+++ game.js
@@ -0,0 +1,40 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.height=576;
+canvas.width=1024;
+
+const gravity = 0.5;
+
+class Player{
+    constructor(position){
+        this.position = position;
+        this.velocity = {
+            x:0,
+            y:1,
+        }
+        this.height = 100;
+    }
+    draw(){
+        c.fillStyle='red';
+        c.fillRect(this.position.x,this.position.y,100,this.height);
+    }
+    update(){
+        this.draw();
+        this.position.y+=this.velocity.y;
+        if( this.position.y + this.height + this.velocity.y <canvas.height){
+            this.velocity.y+= gravity;
+        }
+        else this.velocity.y=0
+    }
+}
+
+const player = new Player({x:0,y:0});
+function gameloop(){
+    window.requestAnimationFrame(gameloop);
+    c.fillStyle = 'white';
+    c.fillRect(0,0,canvas.width,canvas.height);
+    player.update();
+
+}
+gameloop()
