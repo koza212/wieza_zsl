4655c632a5b1675e2bf83d7fe8529539932d7b41
koza212 <kozlowskimaciej46@gmail.com>
2025-04-02 12:03:34 +0200

    tralalero tralalala

--- src/scripts/core/game.js
+++ src/scripts/core/game.js
@@ -1,48 +1,91 @@
-import { Map } from './../map/map.js';  
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-export class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = 840;
-        this.canvas.height = 840;
+canvas.height=576;
+canvas.width=1024;
 
-        this.map = new Map("../../../assets/map.png");
+const gravity = 0.5;
 
-        this.viewport = {
-            y: 0,
-            width: this.canvas.width,
-            height: this.canvas.height,
-        };
-
-        this.init();
+class Player{
+    constructor(position){
+        this.position = position;
+        this.velocity = {
+            x:0,
+            y:1,
+        }
+        this.height = 100;
+        this.canJump = false;
     }
 
-    async init() {
-        await this.map.load();
-        this.addScrollControls()
-        requestAnimationFrame(() => this.gameLoop());
+    draw(){
+        c.fillStyle='red';
+        c.fillRect(this.position.x,this.position.y,100,this.height);
     }
-
-    addScrollControls() {
-        this.canvas.addEventListener("wheel", (e) => {
-            const scrollSpeed = 50; 
-            if (e.deltaY > 0) {
-                this.viewport.y += scrollSpeed; 
-            } else {
-                this.viewport.y -= scrollSpeed; 
-            }
-
-            // Making srue viewport dont go too high or low
-            this.viewport.y = Math.max(0, Math.min(this.viewport.y, this.map.height - this.viewport.height));
-        });
-    }
-
-
-    gameLoop() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.map.draw(this.ctx, this.viewport);
-
-        requestAnimationFrame(() => this.gameLoop());
+    
+    update(){
+        this.draw();
+        this.position.y+=this.velocity.y;
+        this.position.x+=this.velocity.x;
+        if( this.position.y + this.height + this.velocity.y <canvas.height ){
+            this.velocity.y+= gravity;
+        }
+        else {
+            this.velocity.y=0
+            this.canJump=true;
+        }
     }
 }
+
+const player = new Player({x:0,y:0});
+
+const keys={
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    }
+}
+
+function gameloop(){
+    window.requestAnimationFrame(gameloop);
+    c.fillStyle = 'white';
+    c.fillRect(0,0,canvas.width,canvas.height);
+    player.update();
+
+    player.velocity.x=0;
+    if(keys.d.pressed){
+        player.velocity.x=5;
+    }
+    else if (keys.a.pressed){
+        player.velocity.x=-5;
+    }
+}
+gameloop()
+
+window.addEventListener('keydown',(event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = true;
+            break
+        case 'a':
+            keys.a.pressed = true;
+            break
+        case 'w':
+            if(player.canJump){
+                player.velocity.y=-15;
+                player.canJump=false;
+            }
+            break;
+    }
+})
+window.addEventListener('keyup',(event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = false;
+            break
+        case 'a':
+            keys.a.pressed = false;
+            break
+    }
+})
