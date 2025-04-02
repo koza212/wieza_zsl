f544722817d051fea22143edc439ef3467f84149
koza212 <kozlowskimaciej46@gmail.com>
2025-04-02 18:11:02 +0200

    marek grechuta grechucze jajami (movement 3 (chyba skonczeny i nie trzeba zmieniac okok))

--- src/scripts/core/game.js
+++ src/scripts/core/game.js
@@ -1,91 +1,109 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.height=576;
-canvas.width=1024;
+canvas.height = 576;
+canvas.width = 1024;
 
 const gravity = 0.5;
 
-class Player{
-    constructor(position){
+class Player {
+    constructor(position) {
         this.position = position;
-        this.velocity = {
-            x:0,
-            y:1,
-        }
+        this.velocity = { x: 0, y: 0 };
         this.height = 100;
-        this.canJump = false;
+        this.state = 'walking'; 
+        this.jumpCharge = 0;
+        this.maxJumpCharge = 20;
+        this.lastDirection = 0; // -1 (left), 0 (up), 1 (right)
     }
 
-    draw(){
-        c.fillStyle='red';
-        c.fillRect(this.position.x,this.position.y,100,this.height);
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 100, this.height);
     }
-    
-    update(){
+
+    update() {
         this.draw();
-        this.position.y+=this.velocity.y;
-        this.position.x+=this.velocity.x;
-        if( this.position.y + this.height + this.velocity.y <canvas.height ){
-            this.velocity.y+= gravity;
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
+
+        if (this.state === 'jumping') {
+            if (this.position.y + this.height < canvas.height) {
+                this.velocity.y += gravity;
+            } else {
+                this.velocity.y = 0;
+                this.velocity.x = 0;
+                this.position.y = canvas.height - this.height;
+                this.state = 'walking';
+            }
         }
-        else {
-            this.velocity.y=0
-            this.canJump=true;
-        }
     }
 }
 
-const player = new Player({x:0,y:0});
+const player = new Player({ x: 100, y: canvas.height - 100 });
+const keys = { a: false, d: false };
 
-const keys={
-    d: {
-        pressed: false,
-    },
-    a: {
-        pressed: false,
-    }
-}
-
-function gameloop(){
-    window.requestAnimationFrame(gameloop);
-    c.fillStyle = 'white';
-    c.fillRect(0,0,canvas.width,canvas.height);
-    player.update();
-
-    player.velocity.x=0;
-    if(keys.d.pressed){
-        player.velocity.x=5;
-    }
-    else if (keys.a.pressed){
-        player.velocity.x=-5;
-    }
-}
-gameloop()
-
-window.addEventListener('keydown',(event)=>{
-    switch (event.key){
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
         case 'd':
-            keys.d.pressed = true;
-            break
+            if (player.state === 'walking') {
+                keys.d = true;
+            } else if (player.state === 'charging') {
+                player.lastDirection = 1;
+            }
+            break;
         case 'a':
-            keys.a.pressed = true;
-            break
+            if (player.state === 'walking') {
+                keys.a = true;
+            } else if (player.state === 'charging') {
+                player.lastDirection = -1;
+            }
+            break;
         case 'w':
-            if(player.canJump){
-                player.velocity.y=-15;
-                player.canJump=false;
+            if (player.state === 'walking') {
+                player.state = 'charging';
+                player.velocity.x=0;
+                player.jumpCharge = 0;
             }
             break;
     }
-})
-window.addEventListener('keyup',(event)=>{
-    switch (event.key){
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
         case 'd':
-            keys.d.pressed = false;
-            break
+            keys.d = false;
+            break;
         case 'a':
-            keys.a.pressed = false;
-            break
+            keys.a = false;
+            break;
+        case 'w':
+            if (player.state === 'charging') {
+                player.state = 'jumping';
+                player.velocity.y = -player.jumpCharge;
+                player.velocity.x = player.lastDirection * (player.jumpCharge / 2);
+            }
+            break;
     }
-})
+});
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    
+    if (player.state === 'walking') {
+        player.velocity.x *= 0.7;
+        player.lastDirection=0;
+        if (keys.d) player.velocity.x = 5;
+        if (keys.a) player.velocity.x = -5;
+    }
+
+    if (player.state === 'charging' && player.jumpCharge < player.maxJumpCharge) {
+        player.jumpCharge += 0.5;
+    }
+    
+    player.update();
+}
+
+gameLoop();
