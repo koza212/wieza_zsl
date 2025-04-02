2f79a8b4fee02c743a628c6e23fbad5edc1248e1
koza212 <kozlowskimaciej46@gmail.com>
2025-04-02 07:35:06 +0200

    przyczynek do legendy o św. jerzym - Jacek Kaczmarski (movement 1)

--- game.js
+++ game.js
@@ -22,6 +22,7 @@ class Player{
     update(){
         this.draw();
         this.position.y+=this.velocity.y;
+        this.position.x+=this.velocity.x;
         if( this.position.y + this.height + this.velocity.y <canvas.height){
             this.velocity.y+= gravity;
         }
@@ -30,11 +31,51 @@ class Player{
 }
 
 const player = new Player({x:0,y:0});
+
+const keys={
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    }
+}
+
 function gameloop(){
     window.requestAnimationFrame(gameloop);
     c.fillStyle = 'white';
     c.fillRect(0,0,canvas.width,canvas.height);
     player.update();
 
+    player.velocity.x=0;
+    if(keys.d.pressed){
+        player.velocity.x=5;
+    }
+    else if (keys.a.pressed){
+        player.velocity.x=-5;
+    }
 }
 gameloop()
+
+window.addEventListener('keydown',(event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = true;
+            break
+        case 'a':
+            keys.a.pressed = true;
+            break
+        case 'w':
+            player.velocity.y=-15;
+    }
+})
+window.addEventListener('keyup',(event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed = false;
+            break
+        case 'a':
+            keys.a.pressed = false;
+            break
+    }
+})
